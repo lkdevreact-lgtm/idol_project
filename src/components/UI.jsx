209ef1bf4d3dde832402d88831a39413b66403cf
devreact_js <lkdev.react@gmail.com>
2026-03-25 16:03:27 +0700

    commit code

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -2,12 +2,18 @@ export const UI = () => {
   return (
     <section className="fixed inset-0 z-10 flex items-center justify-center pointer-events-none">
       <div className="absolute top-4 left-4 md:top-8 md:left-14 opacity-0 animate-fade-in-down animation-delay-200 pointer-events-auto">
-        <a href="https://wawasensei.dev" target="_blank">
-          <img
-            src="/images/wawasensei-white.png"
-            alt="Wawa Sensei logo"
-            className="w-20 h-20 object-contain"
-          />
+        <a href="#" target="_blank">
+          <div className="flex items-center gap-3">
+            <img
+              src="/images/logoFibo.png"
+              alt="Wawa Sensei logo"
+              className="w-24 h-24 object-contain"
+            />
+            <div className="flex flex-col gap-1">
+              <p className="text-4xl font-semibold text-[#0fc1f7] tracking-wider">Fibonax</p>
+              <p className="text-lg font-semibold text-[#0fc1f7] tracking-widest">Technologies</p>
+            </div>
+          </div>
         </a>
       </div>
       <div className="absolute left-4 md:left-15 -translate-x-1/2 -rotate-90 flex items-center gap-4 animation-delay-1500 animate-fade-in-down opacity-0">
